42774650234e1f667985cc39c1797383040fa196
Travis-ko <traviskothlow@gmail.com>
2017-10-09 18:30:39 +0000

    commit number 5

--- https_example.js
+++ https_example.js
@@ -6,9 +6,13 @@ var options = {
 };
 
 //called by https whend the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
-}
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
+};
 
 console.log("I'm about to make the request!");
 
